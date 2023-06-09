21d24640202ca5088c301f216b8506d081511f81
Fabricio <fabricio.melo@dcomp.ufs.br>
2023-06-09 14:04:08 -0300

    next.config hearder

--- next.config.js
+++ next.config.js
@@ -4,6 +4,20 @@ const nextConfig = {
 
   images:{
     domains: ["https://images.unsplash.com", "image.tmdb.org", "upload.wikimedia.org", "https://www.sport-histoire.fr", "www.sport-histoire.fr", "sport-histoire.fr", "sport-histoire"]
+  },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
   }
 }
 
--- src/Service/baseAPI.ts
+++ src/Service/baseAPI.ts
@@ -1,23 +1,15 @@
 import axios from 'axios';
-import { useAppStore } from '@/store/store';
+
 const APIS = () =>{
     const baseAPI = axios.create({
         baseURL: process.env.NEXT_PUBLIC_HOST_API,
-        headers:{
-            'Content-Type': "*",
-            'Accept': "*",
-            'Access-Control-Allow-Credentials': true,
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': '*',
-            'Access-Control-Allow-Headers': "*"   
-        }
     });
     
     const TMDBAPI = axios.create({
         baseURL: process.env.BASE_URL,
         params: {
             api_key: process.env.API_KEY,
-            language: "pt-BR"
+            language: "en-US"
         },
     });
 
