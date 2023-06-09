e243c70cc2b10dc14f21650ab01135d5e85d9ecb
Fabricio <fabricio.melo@dcomp.ufs.br>
2023-06-09 14:24:28 -0300

    hotfix: image request prod.

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -9,12 +9,17 @@ interface ICard {
 
 export const Card = ({ image, title, description }: ICard) => {
 
+    const loaderProp =({ src }:any) => {
+        return src;
+    }
+
     return (
         <div className="container cursor-pointer">
             <div className="box hover:shadow-slate-300">
                 {/* <span className="title">{title}</span> */}
                 <div className='flex relative'>
-                    <Image src={`${process.env.NEXT_PUBLIC_IMG_API as string}${image as string} `} alt='novo' width={250} height={100} className='' />
+                    <Image       loader={loaderProp}
+ src={`${process.env.NEXT_PUBLIC_IMG_API as string}${image as string} `} alt='novo' width={250} height={100} className='' />
                     <div className={`duration-1000 absolute z-10 show-description`}>
                         <span className='text-gray-900'>
                         {description}
