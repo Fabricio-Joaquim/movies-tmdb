97ceea84b42a9d5d10f228c71c97c142e2ca440e
Fabricio <fabricio.melo@dcomp.ufs.br>
2023-06-09 12:41:59 -0300

    add: header

--- src/Service/baseAPI.ts
+++ src/Service/baseAPI.ts
@@ -4,12 +4,12 @@ const APIS = () =>{
     const baseAPI = axios.create({
         baseURL: process.env.NEXT_PUBLIC_HOST_API,
         headers:{
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
+            'Content-Type': "*",
+            'Accept': "*",
             'Access-Control-Allow-Credentials': true,
             'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-            'Access-Control-Allow-Headers':'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'   
+            'Access-Control-Allow-Methods': '*',
+            'Access-Control-Allow-Headers': "*"   
         }
     });
     
