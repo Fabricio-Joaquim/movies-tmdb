6edccc27e104e53a0e0cb9ff9d2877bedeb6d836
Fabricio <fabricio.melo@dcomp.ufs.br>
2023-06-09 12:39:20 -0300

    cors resolver deploy

--- src/Service/baseAPI.ts
+++ src/Service/baseAPI.ts
@@ -3,13 +3,21 @@ import { useAppStore } from '@/store/store';
 const APIS = () =>{
     const baseAPI = axios.create({
         baseURL: process.env.NEXT_PUBLIC_HOST_API,
+        headers:{
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Access-Control-Allow-Credentials': true,
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+            'Access-Control-Allow-Headers':'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'   
+        }
     });
     
     const TMDBAPI = axios.create({
         baseURL: process.env.BASE_URL,
         params: {
             api_key: process.env.API_KEY,
-            language: "en-US"
+            language: "pt-BR"
         },
     });
 
