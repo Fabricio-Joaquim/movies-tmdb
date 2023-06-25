ab52ebc3c86cd3cbc8299b7a308e2ce8f2a80b4b
Fabricio <fabricio.melo@dcomp.ufs.br>
2023-06-24 22:15:11 -0300

    build

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,10 @@
-import { Html, Head, Main, NextScript, DocumentProps } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="pt-br">
       <Head>
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&family=Roboto&display=swap" rel="stylesheet" />
-        <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css" />
       </Head>
       <body className='flex h-screen'>
         <Main />
